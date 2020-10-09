ac260fdd75ba0e9d9a05c9338cc4bf29af8bd33f
struanxyz <40318347@live.napier.ac.uk>
2020-10-09 16:43:01 +0100

    Adds new line to program.cs file

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -6,6 +6,7 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Napier!");
             Console.WriteLine("Hello World!");
         }
     }
